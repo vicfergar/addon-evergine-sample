7ce14e3f38e0873b9626098296499c6100b72b36
Victor Ferrer <victorfergar@gmail.com>
2020-08-30 11:14:54 +0200

    Wave Sample server now uses ASPNet Core 3.1.7

--- wave-sample/rootfs/var/www/html/waveengine/mono-config.js
+++
@@ -1,6 +0,0 @@
-config = {
- 	vfs_prefix: "managed",
- 	deploy_prefix: "managed",
- 	enable_debugging: 0,
- 	file_list: [ "Hass_Sample.Web.dll","WaveEngine.Web.dll","WaveEngine.Common.dll","WaveEngine.Mathematics.dll","WaveEngine.Yaml.dll","mscorlib.dll","System.dll","Mono.Security.dll","System.Xml.dll","System.Numerics.dll","System.Core.dll","WebAssembly.Net.WebSockets.dll","netstandard.dll","System.Data.dll","System.Data.DataSetExtensions.dll","System.Drawing.Common.dll","System.IO.Compression.dll","System.IO.Compression.FileSystem.dll","System.ComponentModel.Composition.dll","System.Net.Http.dll","System.Net.Http.WebAssemblyHttpHandler.dll","WebAssembly.Bindings.dll","System.Memory.dll","System.Runtime.Serialization.dll","System.Transactions.dll","System.Xml.Linq.dll","System.Runtime.CompilerServices.Unsafe.dll","WaveEngine.Components.dll","WaveEngine.Framework.dll","WaveEngine.HLSLEverywhere.dll","WaveEngine.Platform.dll","Xamarin.Essentials.dll","Plugin.Connectivity.Abstractions.dll","Plugin.DeviceInfo.dll","Plugin.Connectivity.dll","Hass_Sample.dll","WaveEngine.OpenGL.dll","WaveEngine.Bindings.OpenGL.dll" ],
-}
